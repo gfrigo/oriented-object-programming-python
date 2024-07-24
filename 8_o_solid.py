e59cb55c24240338b7d0a7a633b6a71f13a90683
Gabriel Frigo <gfrigo.sena@gmail.com>
2024-07-24 15:12:05 -0300

    open-close-solid

---
+++ 8_o_solid.py
@@ -0,0 +1,22 @@
+# pylint: disable-all
+
+# Open close principle -> Entradas diferentes geram ações diferentes
+# Molde
+
+class Circo:
+  def apresentacao(self, apresentador : any) -> None:
+    apresentador.__apresentar_show()
+
+class Palhaco:
+  def apresentar_show(self) -> None:
+    print("Palhaço apresentou seu show!")
+
+class Malabarista:
+  def apresentar_show(self) -> None:
+    print("Malabarista apresentou seu show!")
+
+circo = Circo()
+palhaco = Palhaco()
+malabarista = Malabarista()
+
+circo.apresentacao(palhaco)
